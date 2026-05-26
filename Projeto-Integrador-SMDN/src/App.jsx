ad8832ded809fc133b73d1b982519a459b5f5ae4
Adryel <adryel.luis.r@gmail.com>
2026-05-25 22:34:04 -0300

    att2

--- Projeto-Integrador-SMDN/src/App.jsx
+++ Projeto-Integrador-SMDN/src/App.jsx
@@ -1,7 +1,29 @@
+import "./App.css";
+import StatsCard from "./components/StatsCard";
+
 function App() {
   return (
-    <h1>SMDN</h1>
-  )
+    <main className="dashboard">
+      <h1>Dashboard</h1>
+
+      <section className="cards">
+        <StatsCard
+          title="Alertas Ativos"
+          value="4"
+        />
+
+        <StatsCard
+          title="Notificações"
+          value="97.640"
+        />
+
+        <StatsCard
+          title="Ocorrências"
+          value="12"
+        />
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
