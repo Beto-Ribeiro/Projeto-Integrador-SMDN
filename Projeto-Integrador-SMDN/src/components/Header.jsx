d13b8312209965e77a97205ddfe27c404f216cd1
Adryel <adryel.luis.r@gmail.com>
2026-05-26 19:40:27 -0300

    tela report

--- Projeto-Integrador-SMDN/src/components/Header.jsx
+++ Projeto-Integrador-SMDN/src/components/Header.jsx
@@ -1,21 +1,33 @@
 import bellIcon from "../assets/bell.svg";
 
-function Header() {
+function Header({ ativos, notificacoes }) {
   return (
     <header className="header">
-      <div>
-        <h1>Dashboard</h1>
-        <p>
-          Monitoramento em tempo real
-        </p>
+      <div className="header-user">
+        <div className="header-avatar">C</div>
+        <div className="header-user-info">
+          <span className="header-user-name">Carlos H. da Silva</span>
+          <span className="header-user-role">Defesa Civil</span>
+        </div>
+        <button className="header-menu-btn">≡</button>
+        <button className="notification-btn-small">
+          <img src={bellIcon} alt="Notificações" className="bell-icon-small" />
+        </button>
       </div>
 
-      <button className="notification-btn">
-        <img
-          src={bellIcon}
-          alt="Notificações"
-          className="bell-icon"
-        />
+      <div className="header-stats">
+        <div className="header-stat">
+          <span className="header-stat-value red">{ativos}</span>
+          <span className="header-stat-label">Ativos</span>
+        </div>
+        <div className="header-stat">
+          <span className="header-stat-value">{notificacoes}</span>
+          <span className="header-stat-label">Notificações enviadas</span>
+        </div>
+      </div>
+
+      <button className="notification-btn-big">
+        <img src={bellIcon} alt="Notificações" className="bell-icon-big" />
       </button>
     </header>
   );
