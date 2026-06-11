23be04d3816448943ef7d83fe0776ee564235390
TAUBATE - URBANO ALVES DE SOUZA PEREIRA ENGENHEIRO <e014266w10@professor.educacao.sp.gov.br>
2026-06-11 19:01:34 -0300

    Integrando a API do Gemini para o ChatBot

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,15 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'telas/exportador_import.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   await Supabase.initialize(
     url: 'https://robfgvtnoooivihlnomr.supabase.co',
