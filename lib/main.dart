5af6801d192e166f629a51f661643ab7b8d0d493
MURAXXjr <luizgg236@gmail.com>
2026-06-13 11:36:31 -0300

    perfil2.0

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,7 @@ Future<void> main() async {
 
   await Supabase.initialize(
     url: 'https://robfgvtnoooivihlnomr.supabase.co',
-    anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJvYmZndnRub29vaXZpaGxub21yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3Mzg0MjQsImV4cCI6MjA5MTMxNDQyNH0.ne-W9-g5l-Wnx-sYi08GyZplokBPiBy-0HPcLm49-u8',
+    anonKey: 'sb_publishable_eVCeoqzfhoAgkpBnJxzs7w_-PGO0TO9',
   );
 
   runApp(const MeuApp());
@@ -41,17 +40,12 @@ class _TelaPerfilState extends State<TelaPerfil> {
   final double alturaCard = 90;
 
   final nome = TextEditingController();
-  final email = TextEditingController();
-  final telefone = TextEditingController();
   final cpf = TextEditingController();
-
-  final idade = TextEditingController();
-  final peso = TextEditingController();
-  final altura = TextEditingController();
 
   final alergias = TextEditingController();
   final observacoes = TextEditingController();
 
+  String tipoUsuario = "";
   String tipoSanguineo = "O+";
 
   @override
@@ -62,25 +56,39 @@ class _TelaPerfilState extends State<TelaPerfil> {
 
   Future<void> carregarDados() async {
     try {
-      final dados = await supabase.from('Cidadao').select().limit(1).single();
+      // PERFIS
+      final perfil = await supabase.from('Perfis').select().limit(1).single();
+
+      // CIDADAO
+      final cidadao = await supabase.from('Cidadao').select().limit(1).single();
+
+      // HISTORICO
+      final historico = await supabase
+          .from('Historico_Medicacao_Cidadao')
+          .select()
+          .limit(1)
+          .single();
+
+      print(perfil);
+      print(cidadao);
+      print(historico);
 
       setState(() {
-        nome.text = dados['nome'] ?? '';
-        email.text = dados['email'] ?? '';
-        telefone.text = dados['telefone'] ?? '';
-        cpf.text = dados['cpf'] ?? '';
+        // PERFIL
+        nome.text = perfil['prf_nome'] ?? '';
 
-        idade.text = dados['idade'].toString();
-        peso.text = dados['peso'].toString();
-        altura.text = dados['altura'].toString();
+        // CIDADAO
+        cpf.text = cidadao['cid_cpf'] ?? '';
 
-        tipoSanguineo = dados['tipo_sanguineo'] ?? 'O+';
+        // HISTORICO
+        tipoSanguineo = historico['hmc_tipo_sanguineo'] ?? 'O+';
 
-        alergias.text = dados['alergias'] ?? '';
-        observacoes.text = dados['observacoes'] ?? '';
+        alergias.text = historico['hmc_alergias'] ?? '';
+
+        observacoes.text = historico['hmc_doencas_cronicas'] ?? '';
       });
     } catch (e) {
-      debugPrint('Erro ao carregar dados: $e');
+      print(e);
     }
   }
 
@@ -88,9 +96,11 @@ class _TelaPerfilState extends State<TelaPerfil> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFF5F7FA),
+
       body: SafeArea(
         child: SingleChildScrollView(
           padding: const EdgeInsets.all(18),
+
           child: Column(
             children: [
               Row(
@@ -126,33 +136,14 @@ class _TelaPerfilState extends State<TelaPerfil> {
               const SizedBox(height: 20),
 
               campoTexto(nome, 28, true),
-              campoTexto(email, 16, false),
-              campoTexto(telefone, 16, false),
+
+              campoTexto(TextEditingController(text: tipoUsuario), 16, false),
+
               campoTexto(cpf, 16, false),
 
               const SizedBox(height: 20),
 
-              Row(
-                children: [
-                  Expanded(child: campoNumericoBox("Idade", idade)),
-
-                  const SizedBox(width: 8),
-
-                  Expanded(child: campoTipoSanguineo()),
-                ],
-              ),
-
-              const SizedBox(height: 8),
-
-              Row(
-                children: [
-                  Expanded(child: campoNumericoBox("Peso", peso)),
-
-                  const SizedBox(width: 8),
-
-                  Expanded(child: campoNumericoBox("Altura", altura)),
-                ],
-              ),
+              campoTipoSanguineo(),
 
               const SizedBox(height: 30),
 
@@ -161,7 +152,7 @@ class _TelaPerfilState extends State<TelaPerfil> {
 
               const SizedBox(height: 30),
 
-              titulo("Observações"),
+              titulo("Doenças"),
               areaTexto(observacoes),
 
               const SizedBox(height: 30),
@@ -191,9 +182,11 @@ class _TelaPerfilState extends State<TelaPerfil> {
   ) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 4),
+
       child: Text(
         controller.text,
         textAlign: TextAlign.center,
+
         style: TextStyle(
           fontSize: tamanho,
           fontWeight: destaque ? FontWeight.bold : FontWeight.normal,
@@ -203,44 +196,20 @@ class _TelaPerfilState extends State<TelaPerfil> {
     );
   }
 
-  Widget campoNumericoBox(String tituloTxt, TextEditingController controller) {
-    return Container(
-      height: alturaCard,
-      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 6),
-      decoration: BoxDecoration(
-        border: Border.all(color: azulClaro),
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(tituloTxt, style: TextStyle(fontSize: 12, color: azulClaro)),
-
-          const SizedBox(height: 4),
-
-          Text(
-            controller.text,
-            style: TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
-              color: azul,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
   Widget campoTipoSanguineo() {
     return Container(
       height: alturaCard,
+
       padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 6),
+
       decoration: BoxDecoration(
         border: Border.all(color: azulClaro),
         borderRadius: BorderRadius.circular(12),
       ),
+
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
+
         children: [
           Text(
             "Tipo sanguíneo",
@@ -251,6 +220,7 @@ class _TelaPerfilState extends State<TelaPerfil> {
 
           Text(
             tipoSanguineo,
+
             style: TextStyle(
               fontSize: 22,
               fontWeight: FontWeight.bold,
@@ -265,6 +235,7 @@ class _TelaPerfilState extends State<TelaPerfil> {
   Widget titulo(String texto) {
     return Text(
       texto,
+
       style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold, color: azul),
     );
   }
@@ -272,14 +243,19 @@ class _TelaPerfilState extends State<TelaPerfil> {
   Widget areaTexto(TextEditingController controller) {
     return Container(
       width: double.infinity,
+
       margin: const EdgeInsets.only(top: 10),
+
       padding: const EdgeInsets.all(12),
+
       decoration: BoxDecoration(
         border: Border.all(color: azulClaro),
         borderRadius: BorderRadius.circular(12),
       ),
+
       child: Text(
         controller.text.isEmpty ? "Nenhuma informação" : controller.text,
+
         style: TextStyle(fontSize: 16, color: azul),
       ),
     );
